9a3ff76fa95629ca8245d57b5eb9f170bfbc3381
Andrey Shobonov <lebmed9@gmail.com>
2022-06-30 18:44:51 -0600

    1

--- src/store/actions.ts
+++ src/store/actions.ts
@@ -2,11 +2,6 @@ export const CardActionsType = {
   setCardName: 'BurgerMenu',
 };
 
-// export const SetCardAction = (name: string) => ({
-//   type: CardActionsType.setCardName,
-//   payload: name,
-// });
-
 export const SetCardAction = (burgerState: boolean) => ({
   type: CardActionsType.setCardName,
   payload: burgerState,
